ac40efa28f8578f397852d16c32b71df62aa9330
L0L022 <L0L022@openmailbox.org>
2017-09-27 20:20:13 +0200

    cpp tests mieux

--- cpp/ColorImage_test.cpp
+++ cpp/ColorImage_test.cpp
@@ -5,34 +5,21 @@
 using namespace std;
 
 int main() {
-  ColorImage empty(10, 10);
-  empty.fill(1);
-  empty.pixel(0, 0) = 10;
-  empty.pixel(5, 2) = 10;
-  empty.pixel(9, 9) = 10;
+  ifstream catFile("cat.ppm", std::ios::binary);
+  ColorImage *catImage = ColorImage::readPPM(catFile);
 
-  ColorImage copy(empty);
-  copy.clear();
-  copy.rectangle(0, 0, 5, 5, 12);
+  catImage->rectangle(10, 10, 300, 200, ColorPixel(255, 0, 0));
+  catImage->rectangle(15, 15, 280, 180, ColorPixel(0, 255, 0));
+  catImage->rectangle(20, 20, 270, 170, ColorPixel(0, 0, 255));
 
-  ColorImage copy2(copy);
-  copy2.fillRectangle(5, 5, 2, 2, ColorPixel(255, 0, 0));
-  copy2.pixel(0, 0) = 65;
-  copy2.pixel(1, 0) = 66;
-  copy2.pixel(2, 0) = 67;
-  copy2.pixel(7, 9) = 88;
-  copy2.pixel(8, 9) = 89;
-  copy2.pixel(9, 9) = 90;
+  catImage->fillRectangle(90, 85, 40, 20, ColorPixel(234, 120, 200));
+  catImage->fillRectangle(190, 87, 40, 20, ColorPixel(234, 120, 200));
 
-  cout << empty << endl
-       << copy << endl
-       << copy2 << endl;
+  ofstream catSave("cat_out.ppm", std::ios::binary | std::ios::out);
 
-  fstream save;
-  save.open("out.ppm", std::ios::out | std::ios::binary);
-  if (save.is_open()) {
-  	copy2.writePPM(save);
-  }
+  catImage->writePPM(catSave);
+
+  delete catImage;
 
   return 0;
 }
--- cpp/GreyImage_test.cpp
+++ cpp/GreyImage_test.cpp
@@ -6,48 +6,22 @@ using namespace std;
 
 int main()
 {
-	GreyImage empty(10, 10);
-	empty.fill(1);
-	empty.pixel(0, 0) = 10;
-	empty.pixel(5, 2) = 10;
-	empty.pixel(9, 9) = 10;
+	ifstream catFile("cat.pgm", std::ios::binary);
+  GreyImage *catImage = GreyImage::readPGM(catFile);
 
-	GrayImage copy(empty);
-	copy.clear();
-	copy.rectangle(0, 0, 5, 5, 12);
+  catImage->rectangle(10, 10, 300, 200, 100);
+  catImage->rectangle(15, 15, 280, 180, 250);
+  catImage->rectangle(20, 20, 270, 170, 50);
 
-	GreyImage copy2(copy);
-	copy2.fillRectangle(5, 5, 2, 2, 8);
-	copy2.pixel(0, 0) = 65;
-	copy2.pixel(1, 0) = 66;
-	copy2.pixel(2, 0) = 67;
-	copy2.pixel(7, 9) = 88;
-	copy2.pixel(8, 9) = 89;
-	copy2.pixel(9, 9) = 90;
+  catImage->fillRectangle(90, 85, 40, 20, 10);
+  catImage->fillRectangle(190, 87, 40, 20, 255);
 
-	cout << empty << endl
-			 << copy << endl
-			 << copy2 << endl;
+  ofstream catSave("cat_out.pgm", std::ios::binary | std::ios::out);
 
-	fstream save;
-	save.open("out.pgm", std::ios::out | std::ios::binary);
-	// if (save.is_open()) {
-	// 	copy2.writePGM(save);
-	// }
-	fstream load;
-	load.open("chat.pgm", std::ios::in | std::ios::binary);
-	if (load.is_open()) {
-		GreyImage *image = GreyImage::readPGM(load);
+  catImage->writePGM(catSave);
 
-		cout << std::hex << *image << endl;
+  delete catImage;
 
-		image->fillRectangle(100, 100, 50, 30, 125);
-cout << std::oct;
-		if (save.is_open())
-			image->writePGM(save);
-
-		delete image;
-	}
 
 	return 0;
 }
