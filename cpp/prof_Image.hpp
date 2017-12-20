c46795e34528002567bfe0cc25a28dccbce31904
L0L022 <L0L022@openmailbox.org>
2017-12-20 07:31:36 +0100

    ajoute bonus

--- cpp/prof_Image.hpp
+++ cpp/prof_Image.hpp
@@ -1,2 +1,10 @@
 const char *const identifier = "escales_l";
 const char *const informations = "";
+
+#define CORR_PGMASCII
+#define CORR_PPMASCII
+#define CORR_READCOLORJPEG
+#define CORR_WRITECOLORJPEG
+#define CORR_READCOLORTGARLE
+//#define CORR_BRESENHAM
+#define CORR_TEMPLATE
